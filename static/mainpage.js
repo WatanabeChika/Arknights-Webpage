f9e819a6b53285d52a29d0eef0133a23d15eb51c
WatanabeChika <2606354512@qq.com>
2022-12-31 20:17:34 +0800

    birthday exhibited

--- static/mainpage.js
+++ static/mainpage.js
@@ -9,7 +9,7 @@ function getdata(d, par) {
     return data;
 }
 
-function judge_value(h) {
+function judge_value_toint(h) {
     let i = 0;
     while (h[i]) {
         let j = parseInt(h[i].value);
@@ -24,13 +24,52 @@ function judge_value(h) {
     return h;
 }
 
+function judge_value_todate(d) {
+    let i = 0;
+    while (d[i]) {
+        var j = String(d[i].value);
+        var year = j.indexOf('年') == -1 ? '2022' : j.indexOf('年');
+        var month = j.indexOf('月') == -1 ? '0' : j.slice(j.indexOf('年')+1, j.indexOf('月'));
+        var day = j.slice(j.indexOf('月')+1, j.indexOf('日'));
+        d[i].value = month == '0' ? '未知' : year+'-'+month+'-'+day
+        ++i;
+    }
+    var dataset = new Array();
+    i = 0;
+    while (d[i]) {
+        var exist = false;
+        for (let j = 0; j < dataset.length; ++j) {
+            if (dataset[j][0] == d[i].value) {
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            dataset.push([d[i].value, []]);
+        }
+        ++i;
+    }
+    i = 0;
+    while (d[i]) {
+        for (let j = 0; j < dataset.length; ++j) {
+            if (dataset[j][0] == d[i].value) {
+                var index = j;
+                break;
+            }
+        }
+        dataset[index][1].push(d[i].name);
+        ++i;
+    }
+    return dataset;
+}
+
+// High to low
 function data_sort(d) {
     let res = d.sort(function(a,b){
         return parseInt(b.value) - parseInt(a.value);
     })
     return res;
 }
-
 
 $.getJSON('/static/ArkOperators.json').done(function(data) {
     echarts.init(document.getElementById('height')).setOption({
@@ -44,7 +83,7 @@ $.getJSON('/static/ArkOperators.json').done(function(data) {
         },
         xAxis: [{
             type: 'category',
-            data: data_sort(judge_value(getdata(data, '身高'))).map(item => item.name),
+            data: data_sort(judge_value_toint(getdata(data, '身高'))).map(item => item.name),
             axisLabel: {
                 rotate: 30
             }
@@ -64,18 +103,69 @@ $.getJSON('/static/ArkOperators.json').done(function(data) {
             showBackground: true,
             large: true,
             type: 'bar',
-            data: data_sort(judge_value(getdata(data, '身高'))).map(item => item.value),
+            data: data_sort(judge_value_toint(getdata(data, '身高'))).map(item => item.value),
             label: {
                 show: true,
                 position: 'top',
-                formatter: function(prams) {
-                    if (prams.value == 0) return '未知';
-                    else return prams.value
+                formatter: function(params) {
+                    if (params.value == 0) return '未知';
+                    else return params.value
                 }
             }
         }]
     })
-    // echarts.init(document.getElementById('hair')).setOption({
-
-    // })
+    echarts.init(document.getElementById('birth')).setOption({
+        title: {
+            x: 'center',
+            y: 'top',
+            text: '生日'
+        },
+        tooltip: {
+            formatter: function(params) {
+               var str = params.value[0].slice(5) + '\n\n';
+               let i = 0;
+               while (params.value[1][i]) {
+                str = str + params.value[1][i] + '\n\n';
+                ++i;
+               }
+               return str;
+            }
+        },
+        calendar: [{
+            left: 'center',
+            top: 'middle',
+            cellSize: ['auto', 30],
+            dayLabel: { show: false },
+            yearLabel: {
+                position: 'top',
+                
+            },
+            range: 2022,
+            itemStyle: {
+                color: 'rgba(166, 239, 244, 1)',
+            }
+        }],
+        emphasis: {
+            focus: 'self',
+            label: {
+                fontWeight: 'bolder',
+                fontSize: 20,
+                color: 'rgba(167, 15, 222, 1)'
+            }
+        },
+        series: [{
+            type: 'scatter',
+            coordinateSystem: 'calendar',
+            symbolSize: 0,
+            label: {
+                show: true,
+                formatter: function (params) {
+                    var d = echarts.number.parseDate(params.value[0]);
+                    return d.getDate();
+                },
+                color: '#000'
+            },
+            data: judge_value_todate(getdata(data, '生日'))
+        }]
+    })
 })
